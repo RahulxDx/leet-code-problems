2227d7284f14088361f135c540e2ea2acdf46a86
Rahul R N <138955090+RahulxDx@users.noreply.github.com>
2024-10-12 13:02:52 +0530

    Update Sort Colors.py

--- Sort Colors.py
+++ Sort Colors.py
@@ -1,8 +1,14 @@
-class Solution:
+''' class Solution:
     def sortColors(self, nums: List[int]) -> None:
         for passes in range(len(nums)):
             for j in range(len(nums)-1):
                 if nums[j] > nums[j + 1]:
                     nums[j], nums[j + 1] = nums[j + 1], nums[j]
         return nums
-        
+'''
+
+# Right Answer
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        nums[:] = ([0] * nums.count(0)) + ([1] * nums.count(1)) + ([2] * nums.count(2))
+        return nums
